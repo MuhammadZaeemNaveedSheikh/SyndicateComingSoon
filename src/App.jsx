3ae71f3c9d41a479ad4ceb38f12724080ab6b21c
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-04 19:28:32 +0500

    combine components

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import Gradient from "./assets/bg-gradient.svg"
-import Navbar from "./components/Navbar"
+import Gradient from "./assets/bg-gradient.svg";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <main>
-      <img src={Gradient} className="absolute left-1/2 -translate-x-1/2 top-0 md:-top-32" />
+    <main className="font-['Poppins'] text-white mx-5 flex flex-col min-h-screen justify-between">
+      <img
+        src={Gradient}
+        className="absolute left-1/2 -translate-x-1/2 top-0 md:-top-32 -z-10"
+      />
       <Navbar />
+      <Body />
+      <Footer />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
