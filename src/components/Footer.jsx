586cca899e8f45fa4e63fb6b36d4ad02db7bc70d
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-04 19:28:03 +0500

    Add footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="py-2 w-full">
+      <p className="font-light text-center opacity-75 text-sm md:text-base">2024 © All rights reserved by Syndicate Funded.</p>
+    </footer>
+  );
+};
+
+export default Footer;
