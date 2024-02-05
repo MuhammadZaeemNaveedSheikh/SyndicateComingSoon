aaaea3b3c54c52cd649d32d9d6dcd0c6afe54d2e
Abdul Ahad Qureshi <97045542+AbdulAhadQureshi1@users.noreply.github.com>
2024-02-05 22:00:02 +0500

    Add icons to accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,15 +1,37 @@
 const Accordion = () => {
+  const perks = [
+    {
+      statement: "📈 6% Profit Target Phase 1 and Phase2",
+    },
+    {
+      statement: "💰 10% overall Drawdown, 4% Daily",
+    },
+    {
+      statement: "⏳ 4 Minimum Days",
+    },
+    {
+      statement: "💵 Up to 90% Payouts",
+    },
+    {
+      statement: "🤖 HFT Model",
+    },
+    {
+      statement: "🔄 EAs allowed",
+    },
+    {
+      statement: "🚫 No hidden rules",
+    },
+  ];
 
   return (
     <div className="p-[1px] mt-2 bg-white bg-opacity-5 rounded-md backdrop-blur-sm gradient-border mb-5">
-      <ul className="p-5 text-gray-300 bg-[#0E0C14]">
-        <li>6% Profit Target Phase 1 and Phase2</li>
-        <li>10% overall Drawdown, 4% Daily</li>
-        <li>4 Minimum Days</li>
-        <li>Up to 90% Payouts</li>
-        <li>HFT Model</li>
-        <li>EAs allowed</li>
-        <li>No hidden rules</li>
+      <ul className="p-5 bg-[#0E0C14]">
+        {perks.map((perk, index) => (
+          <li key={index} className="flex gap-3 mb-1">
+            {/* <img src={perk.icon} className={`w-5 h-5`} /> */}
+            <p>{perk.statement}</p>
+          </li>
+        ))}
       </ul>
     </div>
   );
